25a6ce64f08221c3ba97ea999a8893b816bc7e7e
sayantann11 <sayantann11@gmail.com>
2026-04-05 17:06:45 +0530

    move runtime.txt to website

--- website/main.py
+++ website/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 
@@ -13,4 +14,5 @@ def test():
     return jsonify({"message": "Backend working"})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
